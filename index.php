2f0a7b71285b8861b89328d8ae4a7c39280e91f1
romanbI4 <vdov.romanoff@gmail.com>
2020-11-10 13:35:41 +0200

    Refactor code for other task

--- index.php
+++ index.php
@@ -1,29 +1,26 @@
 <?php 
+
 function happyTickets($start, $end)
 {
     if (!empty($start) && !empty($end)) {
-        $range = rand($start, $end);
-        $arrayOfRange = str_split($range);
-        if (count($arrayOfRange) == 6) {
-            $firstPartArrayOfRange = $arrayOfRange[0] + $arrayOfRange[1] + $arrayOfRange[2];
-            $secondPartArrayOfRange = $arrayOfRange[3] + $arrayOfRange[4] + $arrayOfRange[5];
-            if ($firstPartArrayOfRange == $secondPartArrayOfRange && strlen($secondPartArrayOfRange) != 2) {
-                echo $range . " - билет счастливый!";
-            } elseif ($firstPartArrayOfRange != $secondPartArrayOfRange && strlen($secondPartArrayOfRange) == 2) {
-                $secondSplit = str_split($secondPartArrayOfRange);
-                $resultSplit = $secondSplit[0] + $secondSplit[1];
-                if ($firstPartArrayOfRange == $resultSplit) {
-                    echo $range . " - билет счастливый!";
-                } else {
-                    echo $range . " - билет не счастливый!";
+        $sumHappyTickets = [];
+        for ($i = $start; $i <= $end; $i++) {
+            $arrayOfRange = str_split($i);
+            if (count($arrayOfRange) == 6) {
+                $firstPartArrayOfRange = $arrayOfRange[0] + $arrayOfRange[1] + $arrayOfRange[2];
+                $secondPartArrayOfRange = $arrayOfRange[3] + $arrayOfRange[4] + $arrayOfRange[5];
+                if ($firstPartArrayOfRange == $secondPartArrayOfRange && strlen($secondPartArrayOfRange) != 2) {
+                    $sumHappyTickets[$i]++;
+                } elseif ($firstPartArrayOfRange != $secondPartArrayOfRange && strlen($secondPartArrayOfRange) == 2) {
+                    $secondSplit = str_split($secondPartArrayOfRange);
+                    $resultSplit = $secondSplit[0] + $secondSplit[1];
+                    if ($firstPartArrayOfRange == $resultSplit) {
+                        $sumHappyTickets[$i]++;
+                    } 
                 }
-            } else {
-                echo $range . " - билет не счастливый!";
-            }
-        } else {
-            array_unshift($arrayOfRange, "0");
-            happyTickets($start, $end);
+            } 
         }
+        echo "Счастливых билетов в диапозоне с " . $start . " до " . $end . " - " . "<b>" . count($sumHappyTickets) . "</b>";
     } else {
         echo 'Ошибка, не задан один из параметров';
     }
